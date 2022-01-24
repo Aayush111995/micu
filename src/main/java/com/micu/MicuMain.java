7bb77aebdcbdca1a25714f3e95c185bb803de9bb
Aayush Chaudhary <aayush.chaudhary@cleartrip.com>
2022-01-24 22:44:21 +0530

    pushing initial setup

---
+++ src/main/java/com/micu/MicuMain.java
@@ -0,0 +1,12 @@
+package com.micu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicuMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicuMain.class, args);
+    }
+}
