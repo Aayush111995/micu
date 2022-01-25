c1d5ab3a16171d484ef8abf47160da4a281115a0
umang <umangjangid777@gmail.com>
2022-01-25 13:56:36 +0530

    jai ho

--- src/main/java/com/micu/MicuMain.java
+++ src/main/java/com/micu/MicuMain.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MicuMain {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MicuMain.class, args);
     }
+
 }
